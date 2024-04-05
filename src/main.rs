60c07e04c942501ee9775535bb6db1393940a1b4
BasmalaAB <Basmalaab@aucegypt.edu>
2024-04-06 03:20:44 +0400

    Info extraction from packet: TCP Socket
    Function that extracts info from the TCP Socket, it's source and destination ports.
    
    Added necessary dependencies in Cargo.toml file.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
 extern crate pcap;
 use pcap::Device;
+use pnet::packet::tcp::TcpPacket;
+
+
+fn extract_tcp_socket(packet: &[u8]) -> Option<(u16,u16)>{
+	if let Some(tcp) = TcpPacket::new(packet) {
+		let src_port = tcp.get_source();
+		let dst_port = tcp.get_destination();
+		//let src_IP = tcp.get_ //gets IP address of sender
+		//get IP Address of receiver
+		Some((src_port,dst_port))
+	}
+	else {
+	None
+	}
+}
+
 
 fn capture_packets()
 {
+
      let mut cap = Device::lookup().unwrap().unwrap().open().unwrap();
-   
+    
     while let Ok(packet) = cap.next_packet() {
-                println!("recieved packet! {:?}", packet);
+                match extract_tcp_socket(&packet){
+                	Some((src_port, dst_port)) => {
+                		println!("Source Port: {}, Destination Port: {}", src_port, dst_port);
+                	},
+                	None => println!("TCP Socket not found in the packet header"),
+                }
             }
+
 }
 
 fn main() {
